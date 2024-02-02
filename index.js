8e7ec75442e61c49ce8812210eda6480c7722c5c
Suraj Pandey <surazpandey101@gmail.com>
2024-02-02 09:35:52 +0545

    learning to build an api

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const app = express();
 
 const router = require("./routes/route");
 const PORT = 4000;
-const apiVersion = "api/v1";
-app.use(`/${apiVersion}`, router);
+const apiVersion = "/api/v1";
+app.use(`${apiVersion}`, router);
 
-app.use((error) => {});
+app.use((error) => {
+  const err = err ? err.toString() : "something went wrong";
+  res.status(500).json({ message: err });
+});
 app.listen(PORT, (req, res) => {
   console.log(`The server is running on http://localhost:${PORT}`);
 });
